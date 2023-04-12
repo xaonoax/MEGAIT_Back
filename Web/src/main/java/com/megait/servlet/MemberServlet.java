6018ac4e7bd6fc1eed26def71925dcc9709e1ae0
Ji-0 <xaonoax@gmail.com>
2023-04-12 22:24:10 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - Back(Servlet) eclipse로 Member servlet 연동하기

--- Web/src/main/java/com/megait/servlet/MemberServlet.java
+++ Web/src/main/java/com/megait/servlet/MemberServlet.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.megait.db.MemberDAO;
 import com.megait.db.MemberVO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet({"/Member", "/Join"})
 public class MemberServlet extends HttpServlet{
