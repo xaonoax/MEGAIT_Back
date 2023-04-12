e6b6a448190b8a8423d9ceed10654e36000dbfe2
Ji-0 <xaonoax@gmail.com>
2023-04-12 22:21:06 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - Back(Servlet) eclipse로 servlet 연동하기

---
+++ Web/src/main/java/com/megait/servlet/MemberServlet.java
@@ -0,0 +1,60 @@
+package com.megait.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.megait.db.MemberDAO;
+import com.megait.db.MemberVO;
+
+@WebServlet({"/Member", "/Join"})
+public class MemberServlet extends HttpServlet{
+	public MemberServlet() {
+		super();
+		System.out.println("[MemberServlet:MemberServlet()] Instance Created");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("[MemberServlet] New Member, Welcome!");
+		
+		MemberDAO dao = new MemberDAO();
+		MemberVO vo = new MemberVO();
+		int memberno = Integer.parseInt(request.getParameter("memberno"));
+		String id = request.getParameter("id");
+		String name =  request.getParameter("name");
+		vo.setMemberno(memberno);
+		vo.setId(id);
+		vo.setName(name);
+		int result = dao.insert(vo);
+		System.out.println("[MemberServlet:doGet()]" + result + "건이 인서트되었습니다.");
+		
+//		vo.setMemberNo(0);
+//		vo.setId("Jason");
+//		vo.setName("ABCDEFG");
+		ResultSet rs = dao.select(new MemberVO());
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+			while(rs.next()) {
+				out.println("<hr>");
+				out.println(rs.getInt("memberno") + "<br>");
+				out.println(rs.getString("id") + "<br>");
+				out.println(rs.getString("name") + "<br>");
+			}
+		} catch(Exception e) {}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				doGet(request, response);
+	}
+}
